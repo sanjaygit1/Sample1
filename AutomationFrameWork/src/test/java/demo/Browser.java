4ab31ce56a9e363518c84a37acf34904aed33f04
sanjaygit1 <rathinreddy1402@gmail.com>
2025-01-06 07:14:13 +0530

    1

---
+++ AutomationFrameWork/src/test/java/demo/Browser.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new FirefoxDriver();
+		
+
+	}
+
+}
---
+++ AutomationFrameWork/src/test/java/demo/SampleTest.java
@@ -0,0 +1,30 @@
+package demo;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
+public class SampleTest {
+
+	public static void main(String[] args) throws Throwable {
+		
+		Screen scr = new Screen();
+	
+		Pattern mini = new Pattern("C:\\Users\\Shobha\\Pictures\\Screenshots\\MinimizeBar1.png");
+		scr.click(mini);
+	
+		Pattern search = new Pattern("C:\\Users\\Shobha\\Pictures\\Screenshots\\searchBarrr.png");
+		scr.type(search, "NotePad");
+		
+		Robot rob = new Robot();
+		rob.keyPress(KeyEvent.VK_ENTER);
+		rob.keyRelease(KeyEvent.VK_ENTER);
+		
+		Pattern page = new Pattern("C:\\Users\\Shobha\\Pictures\\Screenshots\\screenPage.png");
+		scr.type(page, "Shobha");
+	}
+
+}
